546d826e4baa28111e720839028df9c717b6fcf7
Rob Geada <rob@geada.net>
2023-07-05 11:53:50 +0100

    Add TrustyAI operator to ODH

--- tests/basictests/trustyai.sh
+++ tests/basictests/trustyai.sh
@@ -9,77 +9,78 @@ RESOURCEDIR="${MY_DIR}/../resources"
 
 TEST_USER=${OPENSHIFT_TESTUSER_NAME:-"admin"} #Username used to login to the ODH Dashboard
 TEST_PASS=${OPENSHIFT_TESTUSER_PASS:-"admin"} #Password used to login to the ODH Dashboard
-OPENSHIFT_OAUTH_ENDPOINT="https://$(oc get route -n openshift-authentication   oauth-openshift -o json | jq -r '.spec.host')"
+
 MM_NAMESPACE="${ODHPROJECT}-model"
+
+# trackers of test successes
+REQUESTS_CREATED=false
+FAILURE=false
+FAILURE_HANDLING='FAILURE=true && echo -e "\033[0;31mERROR\033[0m"'
 
 
 os::test::junit::declare_suite_start "$MY_SCRIPT"
 
-function get_authentication(){
-  header "Getting authentication credentials to cluster"
-  oc adm policy add-role-to-user view -n ${ODHPROJECT} --rolebinding-name "view-$TEST_USER" $TEST_USER
-  TESTUSER_BEARER_TOKEN="$(curl -kiL -u $TEST_USER:$TEST_PASS -H 'X-CSRF-Token: xxx' $OPENSHIFT_OAUTH_ENDPOINT'/oauth/authorize?response_type=token&client_id=openshift-challenging-client' | grep -oP 'access_token=\K[^&]*')"
-}
-
-function check_trustyai_resources() {
-  header "Checking that TrustyAI resources have spun up"
-  oc project $ODHPROJECT
-  os::cmd::try_until_text "oc get deployment modelmesh-controller" "modelmesh-controller" $odhdefaulttimeout $odhdefaultinterval
-  os::cmd::try_until_text "oc get deployment trustyai-service" "trustyai-service" $odhdefaulttimeout $odhdefaultinterval
-  os::cmd::try_until_text "oc get route trustyai-service-route" "trustyai-service-route" $odhdefaulttimeout $odhdefaultinterval
-
-  oc wait --for=condition=Ready $(oc get pod -o name | grep trustyai) --timeout=${odhdefaulttimeout}ms
+function setup_monitoring() {
+    header "Enabling User Workload Monitoring on the cluster"
+    oc apply -f ${RESOURCEDIR}/modelmesh/enable-uwm.yaml || eval "$FAILURE_HANDLING"
 }
 
 function deploy_model() {
     header "Deploying model into ModelMesh"
-    oc new-project $MM_NAMESPACE
-    os::cmd::expect_success "oc project $MM_NAMESPACE"
-    os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/modelmesh/service_account.yaml -n ${MM_NAMESPACE}"
+    oc new-project $MM_NAMESPACE || true
+
+    os::cmd::expect_success "oc project $MM_NAMESPACE" || eval "$FAILURE_HANDLING"
+    os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/modelmesh/service_account.yaml -n ${MM_NAMESPACE}" || eval "$FAILURE_HANDLING"
     oc label namespace $MM_NAMESPACE "modelmesh-enabled=true" --overwrite=true || echo "Failed to apply modelmesh-enabled label."
-    os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/trustyai/secret.yaml -n ${MM_NAMESPACE}"
-    os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/trustyai/odh-mlserver-0.x.yaml  -n ${MM_NAMESPACE}"
-#    os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/trustyai/model.yaml  -n ${MM_NAMESPACE}"
+    os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/trustyai/secret.yaml -n ${MM_NAMESPACE}" || eval "$FAILURE_HANDLING"
+    os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/trustyai/odh-mlserver-0.x.yaml  -n ${MM_NAMESPACE}" || eval "$FAILURE_HANDLING"
 
     SECRETKEY=$(openssl rand -hex 32)
-    sed -i "s/<secretkey>/$SECRETKEY/g" ${RESOURCEDIR}/trustyai/sample-minio.yaml
-    os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/trustyai/sample-minio.yaml -n ${MM_NAMESPACE}"
-    #os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/trustyai/openvino-serving-runtime.yaml -n ${MM_NAMESPACE}"
-    os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/trustyai/openvino-inference-service.yaml -n ${MM_NAMESPACE}"
-    sleep 30
+    sed -i "s/<secretkey>/$SECRETKEY/g" ${RESOURCEDIR}/trustyai/sample-minio.yaml || eval "$FAILURE_HANDLING"
+    os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/trustyai/sample-minio.yaml -n ${MM_NAMESPACE}" || eval "$FAILURE_HANDLING"
+    os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/trustyai/openvino-inference-service.yaml -n ${MM_NAMESPACE}" || eval "$FAILURE_HANDLING"
+    os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/trustyai/trustyai_crd.yaml -n ${MM_NAMESPACE}" || eval "$FAILURE_HANDLING"
+}
 
+function check_trustyai_resources() {
+  header "Checking that TrustyAI resources have spun up"
+  oc project $MM_NAMESPACE  || eval "$FAILURE_HANDLING"
+
+  os::cmd::try_until_text "oc get deployment trustyai-service" "trustyai-service" $odhdefaulttimeout $odhdefaultinterval || eval "$FAILURE_HANDLING"
+  os::cmd::try_until_text "oc get route trustyai-service-route" "trustyai-service-route" $odhdefaulttimeout $odhdefaultinterval || eval "$FAILURE_HANDLING"
+  os::cmd::try_until_text "oc get pod | grep trustyai-service" "1/1" $odhdefaulttimeout $odhdefaultinterval || eval "$FAILURE_HANDLING"
 
 }
 
 function check_mm_resources() {
   header "Checking that ModelMesh resources have spun up"
-  oc project $MM_NAMESPACE
+  oc project $MM_NAMESPACE || eval "$FAILURE_HANDLING"
+
+  os::cmd::try_until_text "oc get pod | grep modelmesh-serving" "5/5" $odhdefaulttimeout $odhdefaultinterval || eval "$FAILURE_HANDLING"
   os::cmd::try_until_text "oc get route example-sklearn-isvc" "example-sklearn-isvc" $odhdefaulttimeout $odhdefaultinterval
-  INFER_ROUTE=$(oc get route example-sklearn-isvc --template={{.spec.host}}{{.spec.path}})
-  token=$(oc create token user-one -n ${MM_NAMESPACE})
-  os::cmd::try_until_text "oc get pod | grep modelmesh-serving" "5/5" $odhdefaulttimeout $odhdefaultinterval
-  os::cmd::try_until_text "curl -k https://$INFER_ROUTE/infer -d @${RESOURCEDIR}/trustyai/data.json -H 'Authorization: Bearer $token' -i" "model_name"
+  INFER_ROUTE=$(oc get route example-sklearn-isvc --template={{.spec.host}}{{.spec.path}}) || eval "$FAILURE_HANDLING"
+  token=$(oc create token user-one -n ${MM_NAMESPACE}) || eval "$FAILURE_HANDLING"
+  os::cmd::try_until_text "curl -k https://$INFER_ROUTE/infer -d @${RESOURCEDIR}/trustyai/data.json -H 'Authorization: Bearer $token' -i" "model_name" || eval "$FAILURE_HANDLING"
 }
 
 function check_communication(){
     header "Check communication between TrustyAI and ModelMesh"
-    oc project $MM_NAMESPACE
+    oc project $MM_NAMESPACE || eval "$FAILURE_HANDLING"
 
     # send some data to modelmesh
-    os::cmd::expect_success_and_text "curl -k https://$INFER_ROUTE/infer -d @${RESOURCEDIR}/trustyai/data.json -H 'Authorization: Bearer $token' -i" "model_name"
-    oc project ${ODHPROJECT}
-    os::cmd::try_until_text "oc logs $(oc get pods -o name | grep trustyai-service)" "Received partial input payload" $odhdefaulttimeout $odhdefaultinterval
+    os::cmd::expect_success_and_text "curl -k https://$INFER_ROUTE/infer -d @${RESOURCEDIR}/trustyai/data.json -H 'Authorization: Bearer $token' -i" "model_name" || eval "$FAILURE_HANDLING"
+    os::cmd::try_until_text "oc logs $(oc get pods -o name | grep trustyai-service)" "Received partial input payload" $odhdefaulttimeout $odhdefaultinterval || eval "$FAILURE_HANDLING"
 }
 
 function generate_data(){
     header "Generate some data for TrustyAI (this will take a sec)"
-    oc project $MM_NAMESPACE
+    oc project $MM_NAMESPACE || eval "$FAILURE_HANDLING"
 
     # send a bunch of random data to the model
     DIVISOR=128.498 # divide bash's $RANDOM by this to get a float range of [0.,255.], for MNIST
     for i in {1..500};
     do
-      DATA=$(sed "s/\[40.83, 3.5, 0.5, 0\]/\[$(($RANDOM % 2)),$(($RANDOM / 128)),$(($RANDOM / 128)), $(($RANDOM / 128)) \]/" ${RESOURCEDIR}/trustyai/data.json)
+      DATA=$(sed "s/\[40.83, 3.5, 0.5, 0\]/\[$(($RANDOM % 2)),$(($RANDOM / 128)),$(($RANDOM / 128)), $(($RANDOM / 128)) \]/" ${RESOURCEDIR}/trustyai/data.json) || eval "$FAILURE_HANDLING"
       curl -k https://$INFER_ROUTE/infer -d "$DATA"  -H 'Authorization: Bearer $token' -i > /dev/null 2>&1 &
       sleep .01
     done
@@ -87,66 +88,73 @@ function generate_data(){
 
 function schedule_and_check_request(){
   header "Create a metric request and confirm calculation"
-  oc project $ODHPROJECT
-  TRUSTY_ROUTE=$(oc get route/trustyai --template={{.spec.host}})
+  oc project $MM_NAMESPACE
 
-  os::cmd::expect_success_and_text "curl --location http://$TRUSTY_ROUTE/metrics/spd/request \
+  TRUSTY_ROUTE=https://$(oc get route/trustyai-service --template={{.spec.host}}) || eval "$FAILURE_HANDLING"
+
+  os::cmd::expect_success_and_text "curl -k --location $TRUSTY_ROUTE/metrics/spd/request \
     --header 'Content-Type: application/json' \
     --data '{
         \"modelId\": \"example-sklearn-isvc\",
-        \"protectedAttribute\": \"input-0\",
-        \"favorableOutcome\": {
-            \"type\": \"INT64\",
-            \"value\": 0.0
-        },
-        \"outcomeName\": \"output-0\",
-        \"privilegedAttribute\": {
-            \"type\": \"DOUBLE\",
-            \"value\": 0.0
-        },
-        \"unprivilegedAttribute\": {
-            \"type\": \"DOUBLE\",
-            \"value\": 1.0
-        }
-    }'" "requestId"
-  os::cmd::try_until_text "curl http://$TRUSTY_ROUTE/q/metrics" "trustyai_spd"
+        \"protectedAttribute\": \"predict-0\",
+        \"favorableOutcome\": 0,
+        \"outcomeName\": \"predict\",
+        \"privilegedAttribute\": 0.0,
+        \"unprivilegedAttribute\": 1.0
+    }'" "requestId" || eval "$FAILURE_HANDLING"
+  os::cmd::try_until_text "curl -k $TRUSTY_ROUTE/q/metrics" "trustyai_spd" || eval "$FAILURE_HANDLING"
+  REQUESTS_CREATED=true;
 }
 
 
 function test_prometheus_scraping(){
     header "Ensure metrics are in Prometheus"
-    MODEL_MONITORING_ROUTE=$(oc get route -n ${ODHPROJECT} odh-model-monitoring --template={{.spec.host}})
-    os::cmd::try_until_text "curl -k --location -g --request GET 'https://'$MODEL_MONITORING_ROUTE'//api/v1/query?query=trustyai_spd' -H 'Authorization: Bearer $TESTUSER_BEARER_TOKEN' -i" "value" $odhdefaulttimeout $odhdefaultinterval
+
+    SECRET=`oc get secret -n openshift-user-workload-monitoring | grep  prometheus-user-workload-token | head -n 1 | awk '{print $1 }'` || eval "$FAILURE_HANDLING"
+    TOKEN=`echo $(oc get secret $SECRET -n openshift-user-workload-monitoring -o json | jq -r '.data.token') | base64 -d` || eval "$FAILURE_HANDLING"
+    THANOS_QUERIER_HOST=`oc get route thanos-querier -n openshift-monitoring -o json | jq -r '.spec.host'` || eval "$FAILURE_HANDLING"
+    os::cmd::try_until_text "curl -X GET -kG \"https://$THANOS_QUERIER_HOST/api/v1/query?\" --data-urlencode \"query=trustyai_spd{namespace='opendatahub-model'}\" -H 'Authorization: Bearer $TOKEN' | jq '.data.result[0].metric.protected'" "predict-0" $odhdefaulttimeout $odhdefaultinterval || eval "$FAILURE_HANDLING"
 }
 
 function teardown_trustyai_test() {
   header "Cleaning up the TrustyAI test"
-  oc project $ODHPROJECT
 
-  REQUEST_ID="$(curl http://$TRUSTY_ROUTE/metrics/spd/requests | jq '.requests [0].id')"
+  oc project $MM_NAMESPACE || eval "$FAILURE_HANDLING"
+  TRUSTY_ROUTE=http://$(oc get route/trustyai-service --template={{.spec.host}}) || eval "$FAILURE_HANDLING"
 
-  os::cmd::expect_success_and_text "curl -X DELETE --location http://$TRUSTY_ROUTE/metrics/spd/request \
-    -H 'Content-Type: application/json' \
-    -d '{
-          \"requestId\": \"'"$REQUEST_ID"'\"
-        }'" "Removed"
+  if [ $REQUESTS_CREATED = true ]; then
+    for METRIC_NAME in "spd" "dir"
+    do
+      curl -sk $TRUSTY_ROUTE/metrics/$METRIC_NAME/requests
+      for REQUEST in $(curl -sk $TRUSTY_ROUTE/metrics/$METRIC_NAME/requests | jq -r '.requests [].id')
+      do
+        echo -n $REQUEST": "
+        curl -k -X DELETE --location $TRUSTY_ROUTE/metrics/$METRIC_NAME/request \
+            -H 'Content-Type: application/json' \
+            -d "{
+                  \"requestId\": \"$REQUEST\"
+                }"
+        echo
+      done
+    done
+  fi
 
-  oc project $MM_NAMESPACE
-  os::cmd::expect_success "oc delete -f ${RESOURCEDIR}/trustyai/secret.yaml"
-  os::cmd::expect_success "oc delete -f ${RESOURCEDIR}/trustyai/odh-mlserver-0.x.yaml"
-  os::cmd::expect_success "oc delete -f ${RESOURCEDIR}/trustyai/model.yaml"
-  os::cmd::expect_success "oc delete project $MM_NAMESPACE"
-
+  os::cmd::expect_success "oc delete -f ${RESOURCEDIR}/trustyai/secret.yaml" || eval "$FAILURE_HANDLING"
+  os::cmd::expect_success "oc delete -f ${RESOURCEDIR}/trustyai/odh-mlserver-0.x.yaml" || eval "$FAILURE_HANDLING"
+  os::cmd::expect_success "oc delete -f ${RESOURCEDIR}/trustyai/trustyai_crd.yaml"  || eval "$FAILURE_HANDLING"
+  os::cmd::expect_success "oc delete project $MM_NAMESPACE" || eval "$FAILURE_HANDLING"
 }
 
-get_authentication
-deploy_model
-check_mm_resources
-check_communication
-generate_data
-schedule_and_check_request
-test_prometheus_scraping
+setup_monitoring
+[ $FAILURE = false ] && deploy_model                || echo -e "\033[0;31mSkipping model deployment due to previous failure\033[0m"
+[ $FAILURE = false ] && check_trustyai_resources    || echo -e "\033[0;31mSkipping TrustyAI resource check due to previous failure\033[0m"
+[ $FAILURE = false ] && check_mm_resources          || echo -e "\033[0;31mSkipping ModelMesh resource check due to previous failure\033[0m"
+[ $FAILURE = false ] && check_communication         || echo -e "\033[0;31mSkipping ModelMesh-TrustyAI communication check due to previous failure\033[0m"
+[ $FAILURE = false ] && generate_data               || echo -e "\033[0;31mSkipping data generation due to previous failure\033[0m"
+[ $FAILURE = false ] && schedule_and_check_request  || echo -e "\033[0;31mSkipping metric scheduling due to previous failure\033[0m"
+[ $FAILURE = false ] && test_prometheus_scraping    || echo -e "\033[0;31mSkipping Prometheus data check due to previous failure\033[0m"
 teardown_trustyai_test
 
+[ $FAILURE = true ] && os::cmd::expect_success "echo 'A previous assertion failed, marking suite as failed' && exit 1"
 
 os::test::junit::declare_suite_end
---
+++ trustyai-service-operator/README.md
@@ -0,0 +1,141 @@
+[![Controller Tests](https://github.com/trustyai-explainability/trustyai-service-operator/actions/workflows/controller-tests.yaml/badge.svg)](https://github.com/trustyai-explainability/trustyai-service-operator/actions/workflows/controller-tests.yaml)
+# TrustyAI Kubernetes Operator
+
+## Overview
+
+The TrustyAI Kubernetes Operator aims at simplifying the deployment and management of the [TrustyAI service](https://github.com/trustyai-explainability/trustyai-explainability/tree/main/explainability-service) on Kubernetes and OpenShift clusters by watching for custom resources of kind `TrustyAIService` in the `trustyai.opendatahub.io` API group and manages deployments, services, and optionally, routes and `ServiceMonitors` corresponding to these resources.
+
+The operator ensures the service is properly configured, is discoverable by Prometheus for metrics scraping (on both Kubernetes and OpenShift), and is accessible via a Route on OpenShift.
+
+## Prerequisites
+
+- Kubernetes cluster v1.19+ or OpenShift cluster v4.6+
+- `kubectl` v1.19+ or `oc` client v4.6+
+
+## Installation using pre-built Operator image
+
+This operator is available as an [image on Quay.io](https://quay.io/repository/trustyai/trustyai-service-operator?tab=history). 
+To deploy it on your cluster:
+
+1. **Install the Custom Resource Definition (CRD):**
+
+   Apply the CRD to your cluster (replace the URL with the relevant one, if using another repository):
+
+    ```bash
+    kubectl apply -f https://raw.githubusercontent.com/trustyai-explainability/trustyai-service-operator/main/config/crd/bases/trustyai.opendatahub.io.trustyai.opendatahub.io_trustyaiservices.yaml
+    ```
+
+2. **Deploy the Operator:**
+
+   Apply the following Kubernetes manifest to deploy the operator:
+
+    ```yaml
+    apiVersion: apps/v1
+    kind: Deployment
+    metadata:
+      name: trustyai-operator
+      namespace: trustyai-operator-system
+    spec:
+      replicas: 1
+      selector:
+        matchLabels:
+          control-plane: trustyai-operator
+      template:
+        metadata:
+          labels:
+            control-plane: trustyai-operator
+        spec:
+          containers:
+            - name: trustyai-operator
+              image: quay.io/trustyai/trustyai-service-operator:latest
+              command:
+                - /manager
+              resources:
+                limits:
+                  cpu: 100m
+                  memory: 30Mi
+                requests:
+                  cpu: 100m
+                  memory: 20Mi
+    ```
+
+   or run
+
+   ```shell
+   kubectl apply -f https://raw.githubusercontent.com/trustyai-explainability/trustyai-service-operator/main/artifacts/examples/deploy-operator.yaml   
+   ```
+
+## Usage
+
+Once the operator is installed, you can create `TrustyAIService` resources, and the operator will create corresponding TrustyAI deployments, services, and (on OpenShift) routes.
+
+Here's an example `TrustyAIService` manifest:
+
+```yaml
+apiVersion: trustyai.opendatahub.io.trusty.opendatahub.io/v1
+kind: TrustyAIService
+metadata:
+  name: trustyai-service-example
+spec:
+  storage:
+    format: "PVC"
+    folder: "/inputs"
+    size: "1Gi"
+  data:
+    filename: "data.csv"
+    format: "CSV"
+  metrics:
+    schedule: "5s"
+    batchSize: 5000 # Optional, defaults to 5000
+```
+
+You can apply this manifest with 
+
+```shell
+kubectl apply -f <file-name.yaml> -n $NAMESPACE
+```
+to create a service, where `$NAMESPACE` is the namespace where you want to deploy it.
+
+
+Additionally, in that namespace:
+
+* a `ServiceMonitor` will be created to allow Prometheus to scrape metrics from the service.
+* (if on OpenShift) a `Route` will be created to allow external access to the service.
+
+### Custom Image Configuration using ConfigMap
+
+You can configure the operator to use custom images by creating a `ConfigMap` in the operator's namespace. 
+The operator only checks the ConfigMap at deployment, so changes made afterward won't trigger a redeployment of services.
+
+Here's an example of a ConfigMap that specifies a custom image:
+
+```yaml
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: trustyai-service-operator-config
+data:
+  trustyaiServiceImageName: 'quay.io/mycustomrepo/mycustomimage'
+  trustyaiServiceImageTag: 'v1.0.0'
+```
+
+You can apply this manifest with the following command, replacing `<file-name.yaml>` with the name of your manifest file:
+
+```shell
+kubectl apply -f <file-name.yaml> -n $OPERATOR_NAMESPACE
+```
+
+Please ensure the namespace specified is the same as the namespace where you have deployed the operator.
+
+After the ConfigMap is applied, you can then proceed to deploy the TrustyAI service.
+The operator will use the image name and tag specified in the ConfigMap for the deployment.
+
+If you want to use a different image or tag in the future, you'll need to update the ConfigMap and redeploy the operator to have the changes take effect. The running TrustyAI services won't be redeployed automatically. To use the new image or tag, you'll need to delete and recreate the TrustyAIService resources.
+
+## Contributing
+
+Please see the [CONTRIBUTING.md](./CONTRIBUTING.md) file for more details on how to contribute to this project.
+
+## License
+
+This project is licensed under the Apache License Version 2.0 - see the [LICENSE](./LICENSE) file for details.
